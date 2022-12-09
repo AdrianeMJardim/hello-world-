fe11e4804794575c7bd23c8370357b131845add9
Adriane Miranda <dri.mirian@gmail.com>
2022-12-08 21:26:29 -0300

    blah

---
+++ helloworld/src/App.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+import './estilo.css'
+
+function App () {
+  return(
+    <div className="bemvindo">
+      <h1> Hello World </h1>
+    </div>
+  );
+}
+export default App;
